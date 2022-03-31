4ff028e185f0c2e35926a09c4b4e13ad11f57250
Pirogom <mop.pirogom@gmail.com>
2022-03-31 15:47:32 +0900

    menu 예제

--- menu_test.go
+++ menu_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/pirogom/walk"
@@ -9,43 +10,49 @@ import (
 
 // 메뉴 관련
 
-func TestMenu(t *testing.T) {
+func TestMenuMgr(t *testing.T) {
 	wm := walkmgr.NewWin("메뉴 테스트", 640, 480)
 
-	menubar, _ := walk.NewMenu()
+	//
+	m1 := walkmgr.NewMenu("메뉴1")
+	m1.AddAction("액숀1", func() { fmt.Println("액숀1") })
+	m1.AddAction("액숀2", func() { fmt.Println("액숀2") })
+	m1.AddAction("액숀3", func() { fmt.Println("액숀3") })
+	m1.AddAction("액숀4", func() { fmt.Println("액숀4") })
+	m1.AddSeparator()
+	var ca1, ca2 *walk.Action
+	ca1 = m1.AddCheck("체크액션1", true, func() {
+		fmt.Println("체크", ca1.Checked())
+	})
+	ca2 = m1.AddCheck("체크액션2", false, func() {
+		fmt.Println("체크", ca2.Checked())
+	})
+	m1.AddSeparator()
+	m1.AddAction("액숀5", func() { fmt.Println("액숀5") })
+	m1.AddAction("액숀6", func() { fmt.Println("액숀6") })
+
+	s1 := walkmgr.NewMenu("서브메뉴1")
+	s1.AddAction("서브액숀1", func() {})
+	s1.AddAction("서브액숀2", func() {})
+	s1.AddAction("서브액숀3", func() {})
+	s1.AddAction("서브액숀4", func() {})
+	s1.AddAction("서브액숀5", func() {})
+	s1.AddAction("서브액숀6", func() {})
+	s1.AddAction("서브액숀7", func() {})
+
+	ss1 := walkmgr.NewMenu("서브서브메뉴1")
+	ss1.AddAction("서브서브액숀1", func() {})
+	ss1.AddAction("서브서브액숀2", func() {})
+	ss1.AddAction("서브서브액숀3", func() {})
+	ss1.AddAction("서브서브액숀4", func() {})
+	ss1.AddAction("서브서브액숀5", func() {})
+	ss1.AddAction("서브서브액숀6", func() {})
+	s1.AddMenu(ss1)
+
+	m1.AddMenu(s1)
 	//
 
-	menu1 := walk.NewMenuAction(menubar)
-	menu1.SetText("메뉴1")
-
-	act1 := walk.NewAction()
-	act1.SetText("서브메뉴1")
-
-	menubar.Actions().Add(act1)
-
-	act2 := walk.NewAction()
-	act2.SetText("서브메뉴2")
-
-	menubar.Actions().Add(act2)
-
-	sep1 := walk.NewSeparatorAction()
-	menubar.Actions().Add(sep1)
-
-	act3 := walk.NewAction()
-	act3.SetText("서브메뉴3")
-
-	menubar.Actions().Add(act3)
-
-	//
-	submenubar, _ := walk.NewMenu()
-	submenu1 := walk.NewMenuAction(submenubar)
-	submenu1.SetText("섭섭메뉴1")
-
-	menubar.Actions().Add(submenu1)
-	//
-
-	//
-	wm.Window().Menu().Actions().Add(menu1)
+	wm.AddMenu(m1)
 
 	wm.Start()
 }
