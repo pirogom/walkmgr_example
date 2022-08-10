5fb30ee045789a9d742ddc182c8153dd0a2c5b0d
Pirogom <mop.pirogom@gmail.com>
2022-08-10 10:14:09 +0900

    리스트 컨트롤 추가, 컬럼 클릭시 정렬 기능 추가

--- list_control_test.go
+++ list_control_test.go
@@ -9,18 +9,15 @@ import (
 )
 
 // 리스트 컨트롤, 미완성 .. 개발중
-
 func TestListControl(t *testing.T) {
 	wm := walkmgr.NewWin("리스트 컨트롤 테스트", 640, 480)
 
-	lc := walkmgr.NewListControl(wm,
-		walkmgr.LISTCONTROL_CHECKBOX,
-		walkmgr.LISTCONTROL_MULTI_SELECT)
+	lc := walkmgr.NewListControl(wm, &walkmgr.ListControlCfg{MultiSelect: true, CheckBox: true})
 
-	lc.AddColumn("이름", 300)
-	lc.AddColumn("나이", 50)
-	lc.AddColumn("성별", 50)
-	lc.AddColumn("상태", 50)
+	lc.AddColumn("이름", 300, walkmgr.LISTCTRL_ALIGN_LEFT, false)
+	lc.AddColumn("나이", 50, walkmgr.LISTCTRL_ALIGN_LEFT, false)
+	lc.AddColumn("성별", 50, walkmgr.LISTCTRL_ALIGN_LEFT, false)
+	lc.AddColumn("상태", 50, walkmgr.LISTCTRL_ALIGN_LEFT, false)
 	lc.Create()
 
 	wm.HComposite()
@@ -31,6 +28,27 @@ func TestListControl(t *testing.T) {
 		lc.SetItemData(idx, 3, "상태")
 	})
 	wm.End()
+
+	wm.Start()
+}
+
+func TestListControlOrdering(t *testing.T) {
+	wm := walkmgr.NewWin("리스트 컨트롤 테스트", 640, 480)
+
+	lc := walkmgr.NewListControl(wm, &walkmgr.ListControlCfg{MultiSelect: true, CheckBox: true})
+
+	lc.AddColumn("이름", 300, walkmgr.LISTCTRL_ALIGN_LEFT, true)
+	lc.AddColumn("나이", 50, walkmgr.LISTCTRL_ALIGN_LEFT, true)
+	lc.AddColumn("레벨", 50, walkmgr.LISTCTRL_ALIGN_LEFT, true)
+	lc.Create()
+
+	wm.Starting(func() {
+		for i := 0; i < 10; i++ {
+			nr := lc.AddItemData(fmt.Sprintf("피로곰-%d", i))
+			lc.SetItemData(nr, 1, fmt.Sprintf("%d", i))
+			lc.SetItemData(nr, 2, fmt.Sprintf("%d", 10-i))
+		}
+	})
 
 	wm.Start()
 }
