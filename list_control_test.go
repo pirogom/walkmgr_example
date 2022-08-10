ec50cb1bafd781a499baeb52a9b222f62a1da9a5
Pirogom <mop.pirogom@gmail.com>
2022-08-11 08:33:03 +0900

    ListControl 기능추가

--- list_control_test.go
+++ list_control_test.go
@@ -52,3 +52,31 @@ func TestListControlOrdering(t *testing.T) {
 
 	wm.Start()
 }
+
+func TestListControlItemDblClick(t *testing.T) {
+	wm := walkmgr.NewWin("리스트 컨트롤 테스트", 640, 480)
+
+	lc := walkmgr.NewListControl(wm, &walkmgr.ListControlCfg{MultiSelect: true, CheckBox: true})
+
+	lc.AddColumn("이름", 300, walkmgr.LISTCTRL_ALIGN_LEFT, false)
+	lc.AddColumn("나이", 50, walkmgr.LISTCTRL_ALIGN_LEFT, false)
+	lc.AddColumn("레벨", 50, walkmgr.LISTCTRL_ALIGN_LEFT, false)
+	lc.Create()
+
+	// 더블클릭한 아이템의 정보를 출력
+	lc.ItemDoubleClicked(func(idx int) {
+		fmt.Println(lc.GetItemData(idx, 0))
+		fmt.Println(lc.GetItemData(idx, 1))
+		fmt.Println(lc.GetItemData(idx, 2))
+	})
+
+	wm.Starting(func() {
+		for i := 0; i < 10; i++ {
+			nr := lc.AddItemData(fmt.Sprintf("피로곰-%d", i))
+			lc.SetItemData(nr, 1, fmt.Sprintf("%d", i))
+			lc.SetItemData(nr, 2, fmt.Sprintf("%d", 10-i))
+		}
+	})
+
+	wm.Start()
+}
