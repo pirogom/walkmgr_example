dd04e51d033b898fe627ff08dee839881c22d8c0
Pirogom <mop.pirogom@gmail.com>
2022-12-19 12:07:41 +0900

    add toolbar example

--- toolbar_test.go
+++ toolbar_test.go
@@ -1,28 +1,115 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
-	"github.com/pirogom/walk"
 	"github.com/pirogom/walkmgr"
 )
 
-func TestToolbar1(t *testing.T) {
-	icon1, _ := walk.NewIconFromFile("icon\\icon1.ico")
+func TestDefToolBar(t *testing.T) {
+	wm := walkmgr.NewWin("툴바 테스트(기본)", 640, 480)
 
-	wm := walkmgr.NewWin("툴바 테스트", 640, 480)
+	wm.NewToolBar()
+	wm.AddTool("김", "icon\\icon1.ico", func() {})
+	wm.AddTool("수한무", "icon\\icon2.ico", func() {})
+	wm.ToolBarEnd()
 
-	tb, _ := walk.NewToolBar(wm.Window())
+	wm.Start()
+}
 
-	act1 := walk.NewAction()
-	act1.SetText("하하하하")
-	act1.Triggered().Attach(func() {
-		fmt.Println("하하하하")
-	})
-	act1.SetImage(icon1)
-	tb.Actions().Add(act1)
+func TestTextToolBar(t *testing.T) {
+	wm := walkmgr.NewWin("툴바 테스트(기본 텍스트)", 640, 480)
 
-	wm.Window().SetToolBar(tb)
+	wm.NewToolBarWithText()
+	wm.AddTool("김", "icon\\icon1.ico", func() {})
+	wm.AddTool("수한무", "icon\\icon2.ico", func() {})
+	wm.ToolBarEnd()
+
+	wm.Start()
+}
+
+func TestTextToolBar2(t *testing.T) {
+	wm := walkmgr.NewWin("툴바 테스트(하단 텍스트)", 640, 480)
+
+	wm.NewToolBarWithText2()
+	wm.AddTool("김", "icon\\icon1.ico", func() {})
+	wm.AddTool("수한무", "icon\\icon2.ico", func() {})
+	wm.ToolBarEnd()
+
+	wm.Start()
+}
+
+func TestToolbarWithLine(t *testing.T) {
+	wm := walkmgr.NewWin("툴바 테스트(구분선)", 640, 480)
+
+	wm.NewToolBar()
+	wm.AddTool("김", "icon\\icon1.ico", func() {})
+	wm.AddTool("수한무", "icon\\icon2.ico", func() {})
+	wm.AddToolSeparator()
+	wm.AddTool("거북이", "icon\\icon1.ico", func() {})
+	wm.AddTool("두루미", "icon\\icon2.ico", func() {})
+	wm.AddToolSeparator()
+	wm.AddTool("삼천갑자", "icon\\icon1.ico", func() {})
+	wm.AddTool("동방삭", "icon\\icon2.ico", func() {})
+	wm.AddTool("치치카포", "icon\\icon1.ico", func() {})
+	wm.AddToolSeparator()
+	wm.AddTool("사리사리", "icon\\icon2.ico", func() {})
+	wm.AddTool("센타", "icon\\icon1.ico", func() {})
+	wm.AddTool("워리워리", "icon\\icon2.ico", func() {})
+	wm.AddToolSeparator()
+	wm.AddTool("세브리깡", "icon\\icon1.ico", func() {})
+	wm.AddTool("무두셀라", "icon\\icon2.ico", func() {})
+	wm.ToolBarEnd()
+
+	wm.Start()
+}
+
+func TestToolbarWithLineAndText(t *testing.T) {
+	wm := walkmgr.NewWin("툴바 테스트(구분선+텍스트)", 640, 480)
+
+	wm.NewToolBarWithText()
+	wm.AddTool("김", "icon\\icon1.ico", func() {})
+	wm.AddTool("수한무", "icon\\icon2.ico", func() {})
+	wm.AddToolSeparator()
+	wm.AddTool("거북이", "icon\\icon1.ico", func() {})
+	wm.AddTool("두루미", "icon\\icon2.ico", func() {})
+	wm.AddToolSeparator()
+	wm.AddTool("삼천갑자", "icon\\icon1.ico", func() {})
+	wm.AddTool("동방삭", "icon\\icon2.ico", func() {})
+	wm.AddTool("치치카포", "icon\\icon1.ico", func() {})
+	wm.AddToolSeparator()
+	wm.AddTool("사리사리", "icon\\icon2.ico", func() {})
+	wm.AddTool("센타", "icon\\icon1.ico", func() {})
+	wm.AddTool("워리워리", "icon\\icon2.ico", func() {})
+	wm.AddToolSeparator()
+	wm.AddTool("세브리깡", "icon\\icon1.ico", func() {})
+	wm.AddTool("무두셀라", "icon\\icon2.ico", func() {})
+	wm.ToolBarEnd()
+
+	wm.Start()
+}
+
+func TestToolbarWithLineAndText2(t *testing.T) {
+	wm := walkmgr.NewWin("툴바 테스트(구분선+텍스트)", 640, 480)
+
+	wm.NewToolBarWithText2()
+	wm.AddTool("김", "icon\\icon1.ico", func() {})
+	wm.AddTool("수한무", "icon\\icon2.ico", func() {})
+	wm.AddToolSeparator()
+	wm.AddTool("거북이", "icon\\icon1.ico", func() {})
+	wm.AddTool("두루미", "icon\\icon2.ico", func() {})
+	wm.AddToolSeparator()
+	wm.AddTool("삼천갑자", "icon\\icon1.ico", func() {})
+	wm.AddTool("동방삭", "icon\\icon2.ico", func() {})
+	wm.AddTool("치치카포", "icon\\icon1.ico", func() {})
+	wm.AddToolSeparator()
+	wm.AddTool("사리사리", "icon\\icon2.ico", func() {})
+	wm.AddTool("센타", "icon\\icon1.ico", func() {})
+	wm.AddTool("워리워리", "icon\\icon2.ico", func() {})
+	wm.AddToolSeparator()
+	wm.AddTool("세브리깡", "icon\\icon1.ico", func() {})
+	wm.AddTool("무두셀라", "icon\\icon2.ico", func() {})
+	wm.ToolBarEnd()
+
 	wm.Start()
 }
