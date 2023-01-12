987f305edbc930f5f207f26dba71336e463b5689
Pirogom <mop.pirogom@gmail.com>
2023-01-12 11:03:28 +0900

    treeview edit

--- treeview_test.go
+++ treeview_test.go
@@ -1,21 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/pirogom/walk"
 	"github.com/pirogom/walkmgr"
 )
 
+// under construction
+ 
 func TestTreeView(t *testing.T) {
+
+	icon1, _ := walk.NewIconFromFile("./icon/icon1.ico")
+	icon2, _ := walk.NewIconFromFile("./icon/icon2.ico")
 
 	wm := walkmgr.NewWin("트리뷰 예제", 640, 480)
 
-	treeView := wm.NewTreeView("./icon/icon1.ico", "./icon/icon2.ico")
+	treeView := wm.NewTreeView()
+	item := treeView.AddItem("하하하하", icon1)
+	item2 := item.AddItem("히히히", icon2)
+	item2.AddItem("ㅋㅋㅋㅋ", icon1)
+	treeView.Update()
 
-	item := treeView.AddItem("하하하하", nil)
-	item.AddChildren("히히히", nil)
+	wm.Start()
+}
 
-	treeView.Create()
+func TestTreeView2(t *testing.T) {
+	var addIdx int
+
+	icon1, _ := walk.NewIconFromFile("./icon/icon1.ico")
+	icon2, _ := walk.NewIconFromFile("./icon/icon2.ico")
+
+	wm := walkmgr.NewWin("트리뷰 예제2(추가)", 640, 480)
+
+	treeView := wm.NewTreeView()
+	item := treeView.AddItem("하하하하", icon1)
+	item2 := item.AddItem("히히히", icon2)
+	item2.AddItem("ㅋㅋㅋㅋ", icon1)
+	treeView.Update()
+
+	wm.PushButton("추가", func() {
+		addIdx++
+		treeView.AddItem(fmt.Sprintf("추가됨%d", addIdx), icon1)
+		treeView.Update()
+	})
 
 	wm.Start()
 }
