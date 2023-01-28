5f5672ded930d1556389a015bde1f0d8d9c2973d
Pirogom <mop.pirogom@gmail.com>
2023-01-28 23:02:39 +0900

    treeview update

--- treeview_test.go
+++ treeview_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/pirogom/walk"
@@ -36,7 +37,45 @@ func TestTreeView(t *testing.T) {
 	item4 := treeView.AddItem("2", icon1)
 	item4.AddItem("2-1", icon2)
 
-	treeView.Create()
+	treeView.UpdateItems()
+
+	wm.Start()
+}
+
+func TestTreeView2(t *testing.T) {
+	var itemCnt int
+	icon1, _ := walk.NewIconFromFile("./icon/icon1.ico")
+	icon2, _ := walk.NewIconFromFile("./icon/icon2.ico")
+
+	wm := walkmgr.NewWin("트리뷰 예제", 640, 480)
+
+	treeView := wm.NewTreeView()
+	treeView.AddItem("1", icon1)
+
+	treeView.UpdateItems()
+
+	wm.PushButton("루트 추가", func() {
+		itemCnt++
+		treeView.AddItem(fmt.Sprintf("추가됨%d", itemCnt), icon2)
+		treeView.UpdateItems()
+	})
+
+	wm.PushButton("서브 추가", func() {
+
+		currItem := treeView.CurrentItem()
+
+		if currItem == nil {
+			walkmgr.MsgBox("트리뷰 아이템을 선택하세요.")
+			return
+		}
+
+		itemCnt++
+		currItem.AddItem(fmt.Sprintf("추가된 서브아이템%d", itemCnt), icon2)
+		treeView.UpdateItems()
+		treeView.SetCurrentItem(currItem)
+		treeView.SetExpanded(currItem, true)
+
+	})
 
 	wm.Start()
 }
