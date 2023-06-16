20061b7e062e77d83dd286f6ca0021ffc4b849f4
pirogom <mop.pirogom@gmail.com>
2023-06-17 01:28:02 +0900

    add dialog example

--- dialog_test.go
+++ dialog_test.go
@@ -1,30 +1,41 @@
 package main
 
-// func TestDialog(t *testing.T) {
-// 	wm := walkmgr.NewWin("Dialog", 640, 480)
+import (
+	"testing"
 
-// 	wm.PushButton("다이얼로그", func() {
-// 		// var acceptPB, cancelPB *walk.PushButton
-// 		// dlg, err := walk.NewDialog(wm.Window())
+	"github.com/pirogom/walkmgr"
+)
 
-// 		// if err != nil {
-// 		// 	return
-// 		// }
+func TestDialog(t *testing.T) {
+	wm := walkmgr.NewWin("DialogTest", 640, 480)
 
-// 		// dlg.SetTitle("Test DLG")
-// 		// dlg.SetSize(walk.Size{Width: 300, Height: 200})
-// 		// dlg.SetMinMaxSize(walk.Size{Width: 300, Height: 200}, walk.Size{Width: 300, Height: 200})
-// 		// dlg.SetName("Test DLG")
-// 		// dlg.SetLayout(walk.NewVBoxLayout())
-// 		// dlg.SetDefaultButton(acceptPB)
-// 		// dlg.SetCancelButton(cancelPB)
+	wm.PushButton("다이얼로그", func() {
+		// var acceptPB, cancelPB *walk.PushButton
+		// dlg, err := walk.NewDialog(wm.Window())
 
-// 		// dlg.Run()
+		// if err != nil {
+		// 	return
+		// }
 
-// 		dlg := walkmgr.CreateDialog(wm.Window(), "TestDLG", 300, 300, nil)
+		// dlg.SetTitle("Test DLG")
+		// dlg.SetSize(walk.Size{Width: 300, Height: 200})
+		// dlg.SetMinMaxSize(walk.Size{Width: 300, Height: 200}, walk.Size{Width: 300, Height: 200})
+		// dlg.SetName("Test DLG")
+		// dlg.SetLayout(walk.NewVBoxLayout())
+		// dlg.SetDefaultButton(acceptPB)
+		// dlg.SetCancelButton(cancelPB)
 
-// 		dlg.DoModal()
-// 	})
+		// dlg.Run()
 
-// 	wm.Start()
-// }
+		dlg := walkmgr.NewDialog(wm.Window(), "TestDLG", 300, 300, nil)
+
+		dlg.PushButton("테스트", func() {})
+		dlg.PushButton("테스트1", func() {})
+		dlg.PushButton("테스트2", func() { dlg.Close() })
+		//dlg.IgnoreClosing()
+
+		dlg.Start()
+	})
+
+	wm.Start()
+}
