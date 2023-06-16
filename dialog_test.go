2d89a379f66238253897fb9ec13ff0c7250b8215
pirogom <mop.pirogom@gmail.com>
2023-06-17 00:38:32 +0900

    add dialog_test.go

---
+++ dialog_test.go
@@ -0,0 +1,30 @@
+package main
+
+// func TestDialog(t *testing.T) {
+// 	wm := walkmgr.NewWin("Dialog", 640, 480)
+
+// 	wm.PushButton("다이얼로그", func() {
+// 		// var acceptPB, cancelPB *walk.PushButton
+// 		// dlg, err := walk.NewDialog(wm.Window())
+
+// 		// if err != nil {
+// 		// 	return
+// 		// }
+
+// 		// dlg.SetTitle("Test DLG")
+// 		// dlg.SetSize(walk.Size{Width: 300, Height: 200})
+// 		// dlg.SetMinMaxSize(walk.Size{Width: 300, Height: 200}, walk.Size{Width: 300, Height: 200})
+// 		// dlg.SetName("Test DLG")
+// 		// dlg.SetLayout(walk.NewVBoxLayout())
+// 		// dlg.SetDefaultButton(acceptPB)
+// 		// dlg.SetCancelButton(cancelPB)
+
+// 		// dlg.Run()
+
+// 		dlg := walkmgr.CreateDialog(wm.Window(), "TestDLG", 300, 300, nil)
+
+// 		dlg.DoModal()
+// 	})
+
+// 	wm.Start()
+// }
